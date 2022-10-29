95c50e8dd838cbccf71ca64492ec3bcb3e91541c
sk25469 <sk25469@gmail.com>
2022-10-29 12:05:52 +0530

    testing CI/CD

--- scribble_backend/pkg/model/server_response.go
+++ scribble_backend/pkg/model/server_response.go
@@ -5,6 +5,7 @@ package model
 // "dis "+info.ID+" connectedCLIENTS
 // "set "+info.ID+" "+info.X+" "+info.Y
 
+//	TODO: Change the strucutre of response, to show all the clients connected in the current room
 type ServerResponse struct {
 	ResponseType     string     `json:"response_type"`
 	ID               string     `json:"id"`
