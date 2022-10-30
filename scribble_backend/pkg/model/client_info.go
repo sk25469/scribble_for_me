22bf02ed1b6347beeb56c5bfe45b3bb791e8b4bd
Sahil Sarwar <sk25469@gmail.com>
2022-10-30 12:47:36 +0530

    Delete scribble_backend directory

--- scribble_backend/pkg/model/client_info.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type ClientInfo struct {
-	RoomID   string `json:"room_id"`
-	ClientID string `json:"client_id"`
-	Name     string `json:"name"`
-}
--- scribble_backend/pkg/model/room_info.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Room struct {
-	RoomID string       `json:"room_id"`
-	Group1 []ClientInfo `json:"grp1"`
-	Group2 []ClientInfo `json:"grp2"`
-}
